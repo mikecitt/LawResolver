ba3fe97262aa61fb7187ce79c27e79de9d722183
Milan <mikecitt@gmail.com>
2023-04-14 22:15:21 +0200

    Init java app

---
+++ law-resolver-api/src/main/java/com/ftn/lawresolverapi/LawResolverApiApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.lawresolverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawResolverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawResolverApiApplication.class, args);
+	}
+
+}
---
+++ law-resolver-api/src/test/java/com/ftn/lawresolverapi/LawResolverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.lawresolverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawResolverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
